30a1bf4c3d02a765fb2c6bd946610ad6fb1430e3
Ben Krueger <bennie.goalie@gmail.com>
2017-12-10 15:09:11 -0800

    Add basic save image functionality.

--- traceorama.py
+++ traceorama.py
@@ -2,6 +2,7 @@ import sys, pygame, time, os, io
 from pygame.locals import *
 pygame.init()
 
+pygame.display.set_caption('Traceorama')
 mouse = pygame.mouse
 
 size = width, height = 960, 720
@@ -15,18 +16,29 @@ WHITE = pygame.Color(255,255,255)
 
 canvas.fill(WHITE)
 
-while True:
+quit_pressed = False
+
+while not quit_pressed:
     left_pressed, middle_pressed, right_pressed = mouse.get_pressed()
     for event in pygame.event.get():
         if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
+            quit_pressed = True
+            break
         elif left_pressed:
             pygame.draw.circle(canvas, BLACK, (pygame.mouse.get_pos()), 5)
-    screen.fill(WHITE)
-    screen.blit(canvas, (0,0))
-    pygame.draw.circle(screen, BLACK, (pygame.mouse.get_pos()), 5)
-    pygame.display.update()
+    if not quit_pressed:
+        screen.fill(WHITE)
+        screen.blit(canvas, (0,0))
+        pygame.draw.circle(screen, BLACK, (pygame.mouse.get_pos()), 5)
+        pygame.display.update()
+    else:
+        break
+pygame.image.save(canvas,'/Users/ben/Desktop/traceorama_picture.jpeg')
+pygame.quit()
+sys.exit()
+
+
+
 
 # ball = pygame.image.load("/Users/ben/Desktop/ball.gif")
 # ballrect = ball.get_rect()
