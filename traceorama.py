88c88103c09f3bf7ea08b20bb54e83ba750efecf
Ben Krueger <bennie.goalie@gmail.com>
2017-12-10 14:11:47 -0800

    Added basic drawing capabilities following the tutorial at https://github.com/MiniGirlGeek/pygame-paintme.

--- traceorama.py
+++ traceorama.py
@@ -13,14 +13,19 @@ canvas = screen.copy()
 BLACK = pygame.Color(0,0,0)
 WHITE = pygame.Color(255,255,255)
 
-fill_color = 255, 127, 64
+canvas.fill(WHITE)
 
 while True:
+    left_pressed, middle_pressed, right_pressed = mouse.get_pressed()
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+        elif left_pressed:
+            pygame.draw.circle(canvas, BLACK, (pygame.mouse.get_pos()), 5)
     screen.fill(WHITE)
+    screen.blit(canvas, (0,0))
+    pygame.draw.circle(screen, BLACK, (pygame.mouse.get_pos()), 5)
     pygame.display.update()
 
 # ball = pygame.image.load("/Users/ben/Desktop/ball.gif")
