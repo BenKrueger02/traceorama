9b4d529177f36fddad368df4214e1ab8885016b1
Ben Krueger <bennie.goalie@gmail.com>
2017-12-12 20:30:42 -0800

    Added basic flash image capability.

--- traceorama.py
+++ traceorama.py
@@ -5,6 +5,7 @@ from google.cloud.vision import types
 
 pygame.init()
 
+
 def load_image(file_name):
     with io.open(file_name, 'rb') as image_file:
         content = image_file.read()
@@ -25,6 +26,15 @@ def print_descriptions(descriptions):
     for description in descriptions:
         print(description)
 
+def flash_image(image_file_name, width, height, screen):
+    image = pygame.image.load(image_file_name)
+    image = pygame.transform.scale(image, (width,height))
+    screen.blit(image,(0,0))
+    pygame.display.update()
+    time.sleep(3)
+    screen.fill(WHITE)
+    pygame.display.update()
+
 traceorama_picture_file_name = "traceorama_pictures.jpg"
 client = vision.ImageAnnotatorClient()
 file_name = construct_fullpath('/Users/ben/Downloads/', traceorama_picture_file_name)
@@ -40,6 +50,16 @@ canvas = screen.copy()
 
 BLACK = pygame.Color(0,0,0)
 WHITE = pygame.Color(255,255,255)
+
+canvas.fill(WHITE)
+
+flash_image('/Users/ben/Desktop/Christmas_Tree_Picture.png', width, height, screen)
+# christmas_tree_image = pygame.image.load('/Users/ben/Desktop/Christmas_Tree_Picture.png')
+#     christmas_tree_image = pygame.transform.scale(christmas_tree_image, (width,height))
+#     screen.blit(christmas_tree_image,(0,0))
+#     pygame.display.update()
+#     time.sleep(3)
+#     canvas.fill(WHITE)
 
 canvas.fill(WHITE)
 
@@ -60,6 +80,7 @@ while not quit_pressed:
         pygame.display.update()
     else:
         break
+
 pygame.image.save(canvas, file_name)
 
 image = load_image(file_name)
